b26c48cf47ea330e5867f89b0d53e5efec86aee1
Alexander Cyon <alex.cyon@gmail.com>
2021-08-04 09:54:31 +0200

    SwiftUI CGImage generated from raw images bytes to render Phoenix

---
+++ HoMM2SwiftUI/HoMM2SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoMM2SwiftUIApp.swift
+//  HoMM2SwiftUI
+//
+//  Created by Alexander Cyon on 2021-08-02.
+//
+
+import SwiftUI
+
+@main
+struct HoMM2SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
